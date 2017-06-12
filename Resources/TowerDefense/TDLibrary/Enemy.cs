6f7af8be435492927dd4cf6035dda5d2a8bec2ac
Joseph Schick <jschick04@gmail.com>
2017-06-11 21:13:47 -0500

    Added enemy movement

---
+++ Resources/TowerDefense/TDLibrary/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace TDLibrary {
+
+  public class Enemy : MonoBehaviour {
+    public float speed = 10f;
+
+    private Transform _target;
+    private int _wavepointIndex = 0;
+
+    private void Start() {
+      _target = Waypoints.waypoints[0];
+    }
+
+    private void Update() {
+      Vector3 direction = _target.position - transform.position;
+      transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+
+    }
+  }
+
+}
