6fe726df2b39762fcb30ba5753b93aac4b923f08
Joseph Schick <jschick04@gmail.com>
2017-06-24 16:01:49 -0500

    Created BuildManager and added turret spawning

---
+++ Resources/TowerDefense/TDLibrary/Manager/BuildManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace TDLibrary.Manager {
+
+  public class BuildManager : MonoBehaviour {
+    public static BuildManager instance;
+
+    [SerializeField]
+    private GameObject _basicTurretPrefab;
+    private GameObject _turretToBuild;
+
+    public GameObject GetTurretToBuild() {
+      return _turretToBuild;
+    }
+
+    private void Awake() {
+      if (instance != null) {
+        Destroy(this);
+      }
+      instance = this;
+    }
+
+    private void Start() {
+      _turretToBuild = _basicTurretPrefab;
+    }
+  }
+}
---
+++ Resources/TowerDefense/TDLibrary/Node.cs
@@ -0,0 +1,39 @@
+﻿using TDLibrary.Manager;
+using UnityEngine;
+
+namespace TDLibrary {
+
+  public class Node : MonoBehaviour {
+    [SerializeField]
+    private Color _hoverColor;
+    [SerializeField]
+    private Vector3 _offset;
+    private Renderer _rend;
+    private Color _startColor;
+    private GameObject _turret;
+
+    private void OnMouseDown() {
+      if (_turret != null) {
+        Debug.Log("Cannot build here");
+        return;
+      }
+
+      GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+      _turret = Instantiate(turretToBuild, transform.position + _offset, transform.rotation);
+    }
+
+    private void OnMouseEnter() {
+      _rend.material.color = _hoverColor;
+    }
+
+    private void OnMouseExit() {
+      _rend.material.color = _startColor;
+    }
+
+    private void Start() {
+      _rend = GetComponent<Renderer>();
+      _startColor = _rend.material.color;
+    }
+  }
+
+}
